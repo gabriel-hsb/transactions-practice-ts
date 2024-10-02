359f403e6fb392738dc574955ffe6d3b2d207b1e
Gabriel <ghbranco6@gmail.com>
2024-10-01 21:38:56 -0300

    :tada: first commit

---
+++ dist/fetchData.js
@@ -0,0 +1,4 @@
+export default async function fetchData(URL) {
+    console.log(URL);
+}
+//# sourceMappingURL=fetchData.js.map
---
+++ src/fetchData.ts
@@ -0,0 +1,3 @@
+export default async function fetchData(URL: string) {
+  console.log(URL)
+}
